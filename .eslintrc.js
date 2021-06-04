a399fb90962e0d1fffc1f48b1540156197161952
Adrian Tarasiuk <adrian.tarasiuk23@gmail.com>
2021-06-04 11:28:30 +0200

    Update

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,7 +24,6 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:jsx-a11y/recommended',
-    'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
   ],
   rules: {
